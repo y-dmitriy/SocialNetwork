64009b18b185f5755b1c00a4651bf9b5e295041a
Dmitriy <dmitriy.yushchenko@gmail.com>
2020-05-07 17:56:35 +0300

    Rel les.26

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import { NavLink } from 'react-router-dom';
-
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-
-    return <div className={s.dialog + ' ' + s.active}>
-        <NavLink to={path}>{props.name}</NavLink>
-    </div>;
-}
-
-const Message = (props) => {
-    return <div className={s.dialog}>{props.message}</div>
-}
-
+import DialogItem from './DialogItem/DialogItem.jsx';
+import Message from './Message/Message.jsx';
 
 const Dialogs = (props) => {
 
