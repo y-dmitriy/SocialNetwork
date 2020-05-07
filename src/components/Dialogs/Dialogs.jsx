ba8acad827534f3e1bc8f29927971d60ba3ea7fb
Dmitriy <dmitriy.yushchenko@gmail.com>
2020-05-07 10:14:55 +0300

    Rel les.25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,7 +17,7 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         { id: 1, name: 'Dima' },
         { id: 2, name: 'Ivan' },
         { id: 3, name: 'Petr' },
@@ -26,7 +26,7 @@ const Dialogs = (props) => {
         { id: 6, name: 'Aleksey' }
     ]
 
-    let messagesData = [
+    let messages = [
         { id: 1, message: 'Hi' },
         { id: 2, message: 'How are you?' },
         { id: 3, message: 'I am fine' },
@@ -35,23 +35,17 @@ const Dialogs = (props) => {
         { id: 6, message: 'Yo' }
     ]
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+
+    let messagesElements = messages.map(m => <Message message={m.message} />)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
-                <Message message={messagesData[3].message} />
-                <Message message={messagesData[4].message} />
-                <Message message={messagesData[5].message} />
+                {messagesElements}
             </div>
         </div>
     )
